59600872bdb91ea5687445f8d43175c29ae78405
종 호 <jj0101065@gmail.com>
2020-04-28 15:09:21 +0900

    매퍼 테스트

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -1,5 +1,11 @@
 package org.zerock.mapper;
 
-public interface BoardMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVo;
+
+public interface BoardMapper {
+	@Select("select * from board where BOARD_NO >0")
+	public List<BoardVo>getList();
 }
