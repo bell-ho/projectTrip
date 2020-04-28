0b1237c2c249f08bd1fd7243b202a0ddfe40ef22
seodonguk1128 <seodonguk1128@gmail.com>
2020-04-28 17:46:20 +0900

    reply junitTest 코드 완료

--- src/main/java/org/zerock/mapper/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,5 +1,13 @@
 package org.zerock.mapper;
 
-public interface ReplyMapper {
+import org.zerock.domain.ReplyVo;
 
+public interface ReplyMapper {
+	public int insert(ReplyVo vo); //댓글 작성
+	
+	public ReplyVo read(int reply_no); // 댓글한개 가지고오기
+	
+	public int delete(int reply_no); //댓글 삭제
+	
+	public int update(ReplyVo vo);
 }
