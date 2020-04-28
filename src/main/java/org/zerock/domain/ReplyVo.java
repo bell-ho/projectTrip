263c54f049ccde616d57e212dcc78851e4f4e0e2
종 호 <jj0101065@gmail.com>
2020-04-28 17:57:31 +0900

    서로 테스트

--- src/main/java/org/zerock/domain/ReplyVo.java
+++ src/main/java/org/zerock/domain/ReplyVo.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class ReplyVo {
 	private int REPLY_NO;
-	private int BOARD_NO;
+	private Long BOARD_NO;
 	private int MEM_NO;
 	private String REPLY_CONTENT;
 	private Date REPLY_DATE;
--- src/test/java/org/zerock/mapper/ReplyMapperTest.java
+++ src/test/java/org/zerock/mapper/ReplyMapperTest.java
@@ -17,35 +17,39 @@ public class ReplyMapperTest {
 	@Setter(onMethod_=@Autowired)
 	private ReplyMapper mapper;
 	
-//	@Test Mapper연결 테스트 
-//	public void testMapper() {
-//		log.info(mapper);
-//	}
-//	@Test 댓글등록 테스트 
-//	public void testInsert() {
-//		ReplyVo vo = new ReplyVo();
-//		vo.setBOARD_NO(1);
-//		vo.setMEM_NO(1);
-//		vo.setREPLY_CONTENT("집으로");
-//		log.info("vo : "+vo);
-//		mapper.insert(vo);
-//		
-//	}
-//	@Test 댓글 한개 가지고오기 테스트
-//	public void testGet() {
-//		log.info("결과 "+mapper.read(1));
-//		
-//	}
-//	@Test 댓글 삭제 테스트
-//	public void testDelete() {
-//		log.info("삭제"+mapper.delete(1));
-//	}
-//	@Test 댓글 수정 테스트
-//	public void testUpdate() {
-//		ReplyVo vo = new ReplyVo();
-//		vo.setREPLY_NO(1);
-//		vo.setMEM_NO(1);
-//		vo.setREPLY_CONTENT("수정");
-//		log.info("수정"+mapper.update(vo));
-//	}
+//	@Test //Mapper 연결 테스트 
+	public void testMapper() {
+		log.info(mapper);
+	}
+	
+//	@Test //댓글등록 테스트 
+	public void testInsert() {
+		ReplyVo vo = new ReplyVo();
+		vo.setBOARD_NO(56L);
+		vo.setMEM_NO(1);
+		vo.setREPLY_CONTENT("집으로");
+		log.info("vo : "+vo);
+		mapper.insert(vo);
+		
+	}
+	
+//	@Test //댓글 한개 가지고오기 테스트
+	public void testGet() {
+		log.info("결과 "+mapper.read(1));
+		
+	}
+	
+//	@Test //댓글 삭제 테스트
+	public void testDelete() {
+		log.info("삭제"+mapper.delete(1));
+	}
+	
+//	@Test //댓글 수정 테스트
+	public void testUpdate() {
+		ReplyVo vo = new ReplyVo();
+		vo.setREPLY_NO(1);
+		vo.setMEM_NO(1);
+		vo.setREPLY_CONTENT("수정");
+		log.info("수정"+mapper.update(vo));
+	}
 }
